c6465f5697d6d5bde2d1ecc7adf47188f3d37dd5
Philipp Engel <philipp@filzip.com>
2015-05-18 09:58:50 +0200

    moved request queue delegates to Endpoint

--- app/src/main/java/de/philippengel/android/freshstart/data/Endpoint.java
+++ app/src/main/java/de/philippengel/android/freshstart/data/Endpoint.java
@@ -1,5 +1,6 @@
 package de.philippengel.android.freshstart.data;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 
 import javax.inject.Inject;
@@ -18,5 +19,17 @@ public class Endpoint {
 
     public RequestQueue getQueue() {
         return queue;
+    }
+    
+    public <T> void addToQueue(Request<T> request) {
+        Request<T> add = getQueue().add(request);
+    }
+    
+    public void cancelAll(RequestQueue.RequestFilter filter) {
+        getQueue().cancelAll(filter);
+    }
+    
+    public void cancelAll(Object tag) {
+        getQueue().cancelAll(tag);
     }
 }
